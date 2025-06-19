f5ee530fe86d734c1ac6ee81bdb1da957fc9407e
Zhaofang He <151109521+iwsanren@users.noreply.github.com>
2025-06-19 23:52:46 +0100

    feat: 21 merge two sorted lists

---
+++ linkedlist/src/mergeTwoLists.java
@@ -0,0 +1,32 @@
+
+public class mergeTwoLists {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        // virtual head
+        ListNode dummy = new ListNode(-1), p = dummy;
+        ListNode p1 = list1, p2 = list2;
+
+        while (p1 != null && p2 != null) {
+            // compare the values of current p1 and p2
+            // attach the node with the smaller value to the p pointer
+            if (p1.val < p2.val) {
+                p.next = p1;
+                p1 = p1.next;
+            } else {
+                p.next = p2;
+                p2 = p2.next;
+            }
+            // important: advance the p pointer
+            p = p.next;
+        }
+
+        if (p1 != null) {
+            p.next = p1;
+        }
+
+        if (p2 != null) {
+            p.next = p2;
+        }
+
+        return dummy.next;
+    }
+}
